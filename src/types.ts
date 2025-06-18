a6bacae40923a624e70dea8b8d36449d2b226038
mfgcorpusjr <mcorpus.dev@gmail.com>
2025-06-18 11:21:57 +0800

    data types

---
+++ src/types.ts
@@ -0,0 +1,42 @@
+export type Profile = {
+  id: string;
+  name: string;
+  avatar: string | null;
+  bio: string | null;
+  location: string | null;
+  phone_number: string | null;
+  created_at: Date;
+};
+
+export type Post = {
+  id: number;
+  profile_id: string;
+  text: string;
+  file: string | null;
+  created_at: Date;
+};
+
+export type Comment = {
+  id: number;
+  post_id: number;
+  profile_id: string;
+  text: string;
+  created_at: Date;
+};
+
+export type Like = {
+  id: number;
+  post_id: number;
+  profile_id: string;
+  created_at: Date;
+};
+
+export type Notification = {
+  id: number;
+  sender_id: string;
+  receiver_id: string;
+  title: string;
+  data: string;
+  is_read: boolean;
+  created_at: Date;
+};
