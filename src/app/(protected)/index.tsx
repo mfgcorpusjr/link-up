ec91272d685a801536ae35423f56387d73c7fcdb
mfgcorpusjr <mcorpus.dev@gmail.com>
2025-06-18 11:27:13 +0800

    initial folder structure

--- src/app/(protected)/index.tsx
+++ src/app/(protected)/index.tsx
@@ -1,5 +1,9 @@
+import { Redirect } from "expo-router";
 import { Text } from "react-native";
 
 export default function HomeScreen() {
+  // TODO: remove later
+  return <Redirect href="/welcome" />;
+
   return <Text>Home Screen</Text>;
 }
